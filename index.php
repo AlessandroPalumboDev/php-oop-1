d8feb19856adf2b8498533366b5ec3837367e384
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-24 16:05:01 +0200

    [UPD] Creazione classe con variabili d'istanza e istanziati due oggetti

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// definizione della classe Movie
+class Movie{
+
+    // variabili d'istanza
+    private $titolo;
+    private $anno;
+    private $regista;
+
+}
+
+// oggetti istanziati
+$matrix = new Movie();
+$avatar = new Movie();
+
+
+
+?>
