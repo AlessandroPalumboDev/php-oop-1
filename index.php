306675abb79a552a45f1dcef70799f9dc8a61349
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-06-24 16:46:35 +0200

    [UPD] costruttore e metodi getter e setter

--- index.php
+++ index.php
@@ -3,15 +3,41 @@
 class Movie{
 
     // variabili d'istanza
-    private $titolo;
-    private $anno;
-    private $regista;
+    private string $titolo;
+    private int $anno;
+    private string $regista;
 
+    // definizione di un costruttore
+    public function __construct(int $_anno, string $_titolo){
+        $this->anno = $_anno;
+        $this->titolo = $_titolo;
+    }
+
+    // metodo getter e setter di regista
+    public function getRegista(){
+        return $this->regista;
+    }
+    public function setRegista(string $_regista){
+        $this->regista=$_regista;
+        
+    }
+    
+    
 }
 
 // oggetti istanziati
-$matrix = new Movie();
-$avatar = new Movie();
+
+$matrix = new Movie(1999, 'Matrix');
+// setto regista
+$matrix->setRegista('Andy e Larry Wachowski');
+
+$avatar = new Movie(2009, 'Avatar');
+// setto regista
+$avatar->setRegista('James Cameron');
+
+
+var_dump($matrix);
+var_dump($avatar);
 
 
 
